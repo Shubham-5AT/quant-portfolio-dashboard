947a9cccce6f05f93174e5fcab2ca500188f456a
Shubham <shubham11726@gmail.com>
2026-03-05 15:34:58 +0530

    added comments

--- backend/data_processing.py
+++ backend/data_processing.py
@@ -4,20 +4,20 @@ import pandas as pd
 df = pd.read_csv("../data/data.csv")
 
 df.columns = df.columns.str.strip()          # remove spaces
-df.columns = df.columns.str.replace(" ", "_")
+df.columns = df.columns.str.replace(" ", "_")  #replace spaces with underscore
+#eg - Life Expectancy - Life_Expectancy
 df.columns = df.columns.str.replace("(", "")
 df.columns = df.columns.str.replace(")", "")
-
+#eg - GDP(USD) - GDP_USD
 
 df = df.drop_duplicates()                    # remove duplicate rows
 df = df.fillna(df.mean(numeric_only=True))   # fill numeric missing values
 
 
-df["Year"] = df["Year"].astype(int)
+df["Year"] = df["Year"].astype(int) # Ensure Year column is integer.
 
-numeric_cols = df.select_dtypes(include=["float64","int64"]).columns
-df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors="coerce")
+numeric_cols = df.select_dtypes(include=["float64","int64"]).columns # find all numeric columns
 
-df.to_csv("../data/cleaned_data.csv", index=False)
+df.to_csv("../data/cleaned_data.csv", index=False) #exports
 
 print("Dataset cleaned successfully")
